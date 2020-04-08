1767e387f7ad10bdb49db5b5120e470447ac4199
Alex Kudko <alexkudko@gmail.com>
2020-04-08 17:26:04 +0300

    DB an some functions in controller. Mustache

---
+++ src/main/java/by/kudko/domain/Message.java
@@ -0,0 +1,26 @@
+package by.kudko.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+    public Message(String text, String tag){
+        this.text = text;
+        this.tag = tag;
+    }
+}
