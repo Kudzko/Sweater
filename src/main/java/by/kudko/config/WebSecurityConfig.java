d53e6fade49deb544b6eb2e4121606b061cf74c8
Alex Kudko <alexkudko@gmail.com>
2020-04-10 13:08:58 +0300

    Added roles(User/admin)

--- src/main/java/by/kudko/config/WebSecurityConfig.java
+++ src/main/java/by/kudko/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import by.kudko.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,8 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+//Enable checking rights to access
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
