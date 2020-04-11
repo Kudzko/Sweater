2053e6046c003f0e7bf9d4b58ceb4702739c9149
Alex Kudko <alexkudko@gmail.com>
2020-04-11 13:34:28 +0300

    Added uploading pictures and static content

--- src/main/java/by/kudko/config/MvcConfig.java
+++ src/main/java/by/kudko/config/MvcConfig.java
@@ -1,14 +1,29 @@
 package by.kudko.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Value("${upload.path}")
+    private String uploadPath;
 
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/login").setViewName("login");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Все обращения имеющие в себе /img/** будет перенаправлять все файлы
+        registry.addResourceHandler("/img/**")
+                // by path
+        .addResourceLocations("file://" + uploadPath + "/");
+        // Все обращения имеющие в себе /static/** будет перенаправлять все файлы
+        registry.addResourceHandler("/static/**")
+                // by path. file - for file system, classpath - for looking for by project dir/ tree
+                .addResourceLocations("classpath:/static/");
+    }
 }
--- src/main/java/by/kudko/controller/MainController.java
+++ src/main/java/by/kudko/controller/MainController.java
@@ -5,22 +5,31 @@ import by.kudko.domain.Message;
 import by.kudko.domain.User;
 import by.kudko.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepository messageRepository;
 
+    @Value("${upload.path}")
+    private String uploadPath;
+
     @GetMapping("/")
-    public String greeting( Map<String, Object> model) {
+    public String greeting(Map<String, Object> model) {
 
         return "greeting";
     }
@@ -28,7 +37,7 @@ public class MainController {
     @GetMapping("/main")
     public String main(@RequestParam(required = false) String filter, Model model) {
         Iterable<Message> messages = messageRepository.findAll();
-        if(filter != null && !filter.isEmpty()) {
+        if (filter != null && !filter.isEmpty()) {
             messages = messageRepository.findByTag(filter);
         }
         model.addAttribute("messages", messages);
@@ -40,8 +49,29 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model) {
+            @RequestParam String tag,
+            MultipartFile file,
+            Map<String, Object> model
+    ) throws IOException {
+
         Message msg = new Message(text, tag, user);
+
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                boolean ifCtreatedDir = uploadDir.mkdirs();
+            }
+
+            // to escape collisions we use universally unique identifier(UUID)
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            String uploadFile = uploadDir.getAbsolutePath() + "/" + resultFilename;
+
+            file.transferTo(new File(uploadFile));
+            msg.setFilename(resultFilename);
+        }
         messageRepository.save(msg);
 
         Iterable<Message> messages = messageRepository.findAll();
